7c7e7c447ab20cd68c4d42036358669f926ae728
Daemon19 <komanglangendria04@gmail.com>
2022-06-16 18:49:27 +0800

    🧹 clean up add book handler function

--- src/handler.js
+++ src/handler.js
@@ -5,52 +5,30 @@ const BOOK_ID_SIZE = 16;
 
 const addBookHandler = (request, h) => {
   try {
-    const failResponse = (reason) => h.response({
+    const failResponse = (message) => h.response({
       status: 'fail',
-      message: `Gagal menambahkan buku. ${reason}`,
+      message: `Gagal menambahkan buku. ${message}`,
     }).code(400);
 
-    if (request.payload.name === undefined) return failResponse('Mohon isi nama buku');
+    const book = request.payload;
 
-    const {
-      name,
-      year,
-      author,
-      summary,
-      publisher,
-      pageCount,
-      readPage,
-      reading,
-    } = request.payload;
+    if (book.name === undefined) return failResponse('Mohon isi nama buku');
 
-    if (readPage > pageCount) {
+    if (book.readPage > book.pageCount) {
       return failResponse('readPage tidak boleh lebih besar dari pageCount');
     }
 
-    const id = nanoid(BOOK_ID_SIZE);
-    const finished = pageCount === readPage;
-    const insertedAt = new Date().toISOString();
-    const updatedAt = insertedAt;
+    book.id = nanoid(BOOK_ID_SIZE);
+    book.finished = book.pageCount === book.readPage;
+    book.insertedAt = new Date().toISOString();
+    book.updatedAt = book.insertedAt;
 
-    books.push({
-      name,
-      year,
-      author,
-      summary,
-      publisher,
-      pageCount,
-      readPage,
-      reading,
-      id,
-      finished,
-      insertedAt,
-      updatedAt,
-    });
+    books.push(book);
 
     return h.response({
       status: 'success',
       message: 'Buku berhasil ditambahkan',
-      data: { bookId: id },
+      data: { bookId: book.id },
     }).code(201);
   } catch (err) {
     return h.response({
